e2cc0bb6829adb5fe86f1ebdd87d6f3894c28c2c
kademarcus <kmsp240@uky.edu>
2025-02-18 14:16:09 -0500

    Update starter_regressions.R

--- starter_regressions.R
+++ starter_regressions.R
@@ -27,6 +27,15 @@ logit_residuals
 pearson_residuals_logit <- residuals(mylogit, type = "pearson")
 pearson_residuals_logit
 
+#plotted pearson residuals
+ggplot(fakedata, aes(x = LFitted, y = pearson_residuals_logit)) +
+  geom_point(color = "blue") +                 # Scatter plot of residuals
+  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +  # Horizontal line at 0
+  ggtitle("Pearson Residuals vs. Predicted Probability") +
+  xlab("Predicted Probability") + 
+  ylab("Pearson Residuals") +
+  theme_minimal()
+
 #probit with same variables 
 myprobit <- glm(`Coup` ~ `GDP_per_Capita` + `Military_Expenditure_%_GDP` + `Media_&_Internet_Freedom`, data = fakedata, family = binomial(link = 'probit'))
 summary(myprobit)
@@ -38,3 +47,12 @@ probit_residuals
 #pearson residuals (preferred)
 pearson_residuals_probit <- residuals(myprobit, type = "pearson")
 pearson_residuals_probit
+
+#plotting pearson residuals
+ggplot(fakedata, aes(x = Fitted, y = pearson_residuals_probit)) +
+  geom_point(color = "blue") +                 # Scatter plot of residuals
+  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +  # Horizontal line at 0
+  ggtitle("Pearson Residuals vs. Predicted Probability") +
+  xlab("Predicted Probability") + 
+  ylab("Pearson Residuals") +
+  theme_minimal()
